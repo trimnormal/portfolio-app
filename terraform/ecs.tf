4523e25f45f0be085e04126078a898ed6d215af5
Zachary Corbishley <zacharycorbishley@yahoo.com>
2024-07-28 12:54:35 -0500

    add back state bucket and dynamo table - format

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -62,6 +62,6 @@ resource "aws_ecs_capacity_provider" "this" {
 }
 
 resource "aws_ecs_cluster_capacity_providers" "this" {
-  cluster_name = aws_ecs_cluster.this.name
-  capacity_providers = [ aws_ecs_capacity_provider.this.name ]
+  cluster_name       = aws_ecs_cluster.this.name
+  capacity_providers = [aws_ecs_capacity_provider.this.name]
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -19,43 +19,43 @@ terraform {
   required_version = "~> 1.9.2"
 }
 
-# resource "aws_s3_bucket" "tf-state" {
-#   bucket = "zc-portfolio-app-tf-state"
-# }
+resource "aws_s3_bucket" "tf-state" {
+  bucket = "zc-portfolio-app-tf-state"
+}
 
-# resource "aws_s3_bucket_public_access_block" "this" {
-#   bucket                  = "zc-portfolio-app-tf-state"
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = "zc-portfolio-app-tf-state"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-#   bucket = "zc-portfolio-app-tf-state"
-#   rule {
-#     bucket_key_enabled = false
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = "zc-portfolio-app-tf-state"
+  rule {
+    bucket_key_enabled = false
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 
-# resource "aws_s3_bucket_ownership_controls" "this" {
-#   bucket = aws_s3_bucket.tf-state.id
-#   rule {
-#     object_ownership = "BucketOwnerEnforced"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.tf-state.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
 
-# resource "aws_dynamodb_table" "this" {
-#   name           = "zc-portfolio-app-tf-lock"
-#   read_capacity  = 1
-#   write_capacity = 1
-#   hash_key       = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "this" {
+  name           = "zc-portfolio-app-tf-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
